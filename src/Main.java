cbd652bb93af0ca9bb6c9e9ea6e2a1958cd6b3ef
swami-git <swamy0202@gmail.com>
2018-06-08 15:48:04 +0000

    initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+	    System.out.println("Hello World!");
+	    System.out.println("Hello World!  ");
+	  
+    }
+}
